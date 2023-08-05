6bc9662925fbd8d8293c7b857ee26602ba649c4c
Samuel-Moo <samuelmoo05@gmail.com>
2023-08-05 15:15:50 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(warnings)]
-//add
+
 fn add(param1: i32, param2: i32) -> i32 {
     param1 + param2
 }
 
-//centuryFromYear
+
 fn century_from_year(year: i32) -> i32 {
     let mut century = 1;
     
